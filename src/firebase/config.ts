805e59cdb2e268a00a32691cee2af44ca83ed7f1
Premier Home <contatopremierhome@gmail.com>
2025-12-27 23:15:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2007471545-205ca",
+  "appId": "1:534310101162:web:db5605cb8b813453dcb0d3",
+  "apiKey": "AIzaSyD3RGSaCtNhXLM2qfaB3h3_xWVUOToj0S4",
+  "authDomain": "studio-2007471545-205ca.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "534310101162"
+};
